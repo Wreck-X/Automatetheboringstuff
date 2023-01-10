d55d31c7f0bd06db918bcc28a9c811cf197b9c2c
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-01-10 13:49:18 -0500

    chapter 8 progressed

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import random
-for i in range(random.randint(0,4)):
-    print(i)
+import pyinputplus as pyinp
+breadtype = pyinp.inputMenu(['wheat','white','sourdough'])
+proteintype = pyinp.inputMenu(['chicken','turkey','ham','tofu'])
+yesnocheese = pyinp.inputYesNo('Do you want cheese??? ')
+if yesnocheese == 'yes':
+    cheesetype = pyinp.inputMenu(['cheddar','swiss','mozzerella'])
+howmany = pyinp.inputInt('how many? ',min= 1)
+
+menu = {'wheat':1,'white':1,'sourdough':2,'chicken':5,'turkey':3,'ham':10,'tofu':2,'cheddar':1,'swiss':1,'mozerella':1}
+
+cost = (menu[breadtype] + menu[proteintype] + menu[cheesetype])* howmany
+print('cost = ',cost,'$')
