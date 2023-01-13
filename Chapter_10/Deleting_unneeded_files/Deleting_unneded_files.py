e2d05f6d546edede66f3fdfcde228ac929accf69
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-01-13 07:41:42 -0500

    chapter 10 done

---
+++ Chapter_10/Deleting_unneeded_files/Deleting_unneded_files.py
@@ -0,0 +1,9 @@
+import os,shutil
+from pathlib import Path
+path_ = input('enter absolute path- ')
+os.chdir(path_)
+for folder,subfolder,file in os.walk(path_):
+    for i in file:
+        if os.path.getsize(i) >= 104857600:
+            os.unlink(path_+'/'+i)
+            print(i)
